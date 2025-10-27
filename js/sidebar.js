f6f75cb936e21fd1d149286ce3a0202c79597663
yakoobkhedri <yakkobkhedrii@gmail.com>
2025-10-27 22:54:28 +0330

    first commit

--- js/sidebar.js
+++ js/sidebar.js
@@ -18,3 +18,32 @@ overlay.addEventListener('click' , function () {
   sidebar.classList.remove('active');
   overlay.classList.remove('active');
 });
+
+// tab
+
+let checktab = Array.from(document.getElementsByClassName('checktab'));
+let checktabcontent = Array.from(document.querySelectorAll('#checktabcontent > div'));
+
+checktab.forEach((tab) => {
+    tab.addEventListener('click', function() {
+        // حذف کلاس active از همه تب‌ها
+        checktab.forEach((t) => {
+            t.classList.remove('active');
+        });
+        
+        // اضافه کردن کلاس active به تب کلیک شده
+        tab.classList.add('active');
+        
+        let tabId = tab.dataset.id;
+        
+        // مدیریت محتوای تب‌ها
+        checktabcontent.forEach((content) => {
+            let contentId = content.dataset.id;
+            if (tabId === contentId) {
+                content.classList.add('active');
+            } else {
+                content.classList.remove('active');
+            }
+        });
+    });
+});
